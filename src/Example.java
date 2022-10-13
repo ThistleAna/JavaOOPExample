d22430179b517b89d37d3890dc5022089f1592eb
ANA KHASANAH <ana.khasanah@capita.com>
2022-10-13 09:37:15 +0100

    Creating new objects, person2 and person3

--- src/Example.java
+++ src/Example.java
@@ -16,7 +16,19 @@ public class Example extends Example2 { // this is a class, extends = inherits E
     //main methods
     public static void main(String[] args) {
         Example person1 = new Example(); // creating object person1
+        person1.sayName();
+        person1.sayName("Abdi");
         person1.sayHobby();
+
+        Example person2 = new Example(); //creating object person2
+        person2.sayName();
+        person2.sayName("Zakariya");
+        person2.sayHobby();
+
+        Example person3 = new Example(); // creating object person3
+        person3.sayName();
+        person3.sayName("Ohana");
+        person3.sayHobby();
 
     }
 }
